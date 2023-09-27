2b2f816b80a10a37e007a34e91d6b3bbca0b6273
Mayk <7395296+y0k4i-1337@users.noreply.github.com>
2023-09-27 14:54:52 -0300

    Handle multiple patterns at once

--- popup.js
+++ popup.js
@@ -5,7 +5,49 @@ function removeAccentsAndSpecialChars(str) {
         .replace(/[^a-zA-Z\s]/g, "");
 }
 
-function formatNames(namesArray, formatPattern) {
+// Remove special characters from a string
+function removeSpecialContent(text) {
+    // Remove text enclosed in parentheses
+    text = text.replace(/\([^)]*\)/g, "");
+
+    // Remove text enclosed in square brackets
+    text = text.replace(/\[[^\]]*\]/g, "");
+
+    // Remove text enclosed in curly braces
+    text = text.replace(/\{[^}]*\}/g, "");
+
+    // Remove emojis using a regex (this may not catch all emojis)
+    text = text.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, "");
+
+    return text;
+}
+
+// Remove duplicated spaces
+function removeExtraSpaces(text) {
+    return text.replace(/\s{2,}/g, " ");
+}
+
+// Remove special characters from a string
+function sanitizeFullName(fullName) {
+    let sanitized = removeSpecialContent(fullName.toLowerCase());
+    sanitized = removeAccentsAndSpecialChars(sanitized);
+    sanitized = removeExtraSpaces(sanitized);
+    return sanitized;
+}
+
+// Remove duplicates from an array
+function removeDuplicatesFromArray(array) {
+    // Create a Set from the array to automatically remove duplicates
+    const uniqueSet = new Set(array);
+
+    // Convert the Set to an array and sort it
+    const uniqueArray = Array.from(uniqueSet).sort();
+
+    return uniqueArray;
+}
+
+// Format names using the selected patterns
+function formatNames(namesArray, formatPatterns, domain = "") {
     const formattedNames = [];
 
     for (const fullName of namesArray) {
@@ -14,7 +56,7 @@ function formatNames(namesArray, formatPattern) {
             continue;
         }
 
-        const cleanName = removeAccentsAndSpecialChars(fullName.toLowerCase());
+        const cleanName = sanitizeFullName(fullName);
         const nameParts = cleanName.trim().split(" ");
 
         if (nameParts.length < 2) {
@@ -26,21 +68,24 @@ function formatNames(namesArray, formatPattern) {
         let secondName = nameParts.length > 2 ? nameParts[1] : "";
         let lastName = nameParts[nameParts.length - 1];
 
-        let formattedName = formatPattern
-            .replace("{firstname}", firstName)
-            .replace("{secondname}", secondName)
-            .replace("{lastname}", lastName)
-            .replace("{firstname_firstletter}", firstName.charAt(0))
-            .replace(
-                "{secondname_firstletter}",
-                secondName ? secondName.charAt(0) : ""
-            )
-            .replace("{lastname_firstletter}", lastName.charAt(0));
+        for (const formatPattern of formatPatterns) {
+            let formattedName = formatPattern
+                .replace("{domain}", domain)
+                .replace("{firstname}", firstName)
+                .replace("{secondname}", secondName)
+                .replace("{lastname}", lastName)
+                .replace("{firstname_firstletter}", firstName.charAt(0))
+                .replace(
+                    "{secondname_firstletter}",
+                    secondName ? secondName.charAt(0) : ""
+                )
+                .replace("{lastname_firstletter}", lastName.charAt(0));
 
-        formattedNames.push(formattedName);
+            formattedNames.push(formattedName);
+        }
     }
 
-    return formattedNames;
+    return removeDuplicatesFromArray(formattedNames);
 }
 
 function updateList() {
@@ -49,6 +94,32 @@ function updateList() {
             dataField.value = data.dataList.join("\n");
         }
     });
+}
+
+// Get patterns from the checkboxes and the custom pattern field
+function getPatternList() {
+    const selectedPatterns = []; // Store selected patterns here
+
+    // Loop through the pattern checkboxes
+    document
+        .querySelectorAll(".pattern-checkbox:not(#selectAll)")
+        .forEach((checkbox) => {
+            if (checkbox.checked) {
+                // Get the corresponding label's text (pattern)
+                const label = checkbox.parentElement;
+                selectedPatterns.push(label.textContent.trim());
+            }
+        });
+
+    // Get the custom pattern from the input field
+    const customPattern = document.getElementById("patternField").value.trim();
+
+    // Add the custom pattern to the list if it's not empty
+    if (customPattern) {
+        selectedPatterns.push(customPattern);
+    }
+
+    return selectedPatterns;
 }
 
 //If popup is open, update realtime
@@ -89,9 +160,9 @@ document.addEventListener("DOMContentLoaded", () => {
     exportButton.addEventListener("click", () => {
         let formattedNames = formatNames(
             dataField.value.split("\n"),
-            textPattern.value
+            getPatternList(),
+            textDomain.value
         );
-
         const blob = new Blob([formattedNames.join("\n")], {
             type: "text/plain;charset=utf-8",
         });
